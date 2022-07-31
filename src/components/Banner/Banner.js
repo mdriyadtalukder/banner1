31bc748ff480d33eb097e3f64e82c9084dd41052
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-07-31 20:47:37 +0600

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className="box">
+                <div className="child">
+                    <h1>Wellcome to our page to purchase products</h1>
+                    <button>Purchase</button>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
